1641847be054cda534a6482bab16605688f53559
DavidCavallaro <68825684+DavidCavallaro@users.noreply.github.com>
2021-06-14 21:10:16 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   rootEndpoint: 'http://localhost:3000',
   discordInvite: 'https://discord.gg/uDTgxyg',
   docsURL: 'https://docs.dbots.co',
-  githubURL: 'https://github.com/DavidCavallaro',
+  githubURL: 'https://discord.gg/jxh2qxu',
   version: 'v1.0.3b',
   url: 'https://botlistvabes.herokuapp.com',
   guildId: '810960600015437844'
